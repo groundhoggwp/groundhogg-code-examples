48fac171f377de5d6f66c996ecdea6260573f2da
tobeyadr <gO0UsDYxRwhz>
2019-07-30 09:00:45 -0400

    tag manipulation snippet

---
+++ 2.0/tag-manipulation.php
@@ -0,0 +1,18 @@
+<?php
+
+define( 'ACTIVE_TAG_ID', 1234 );
+define( 'INACTIVE_TAG_ID', 4321 );
+
+add_action( 'groundhogg/contact/tag_applied', function ( \Groundhogg\Contact $contact, int $tag_id ){
+
+    // Ensure that a contact only has 1 of these tags at any given time.
+    switch ( $tag_id ){
+        case ACTIVE_TAG_ID:
+            $contact->remove_tag( INACTIVE_TAG_ID );
+            break;
+        case INACTIVE_TAG_ID:
+            $contact->remove_tag( ACTIVE_TAG_ID );
+            break;
+    }
+
+} );
