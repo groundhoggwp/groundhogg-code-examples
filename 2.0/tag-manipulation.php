02aac67bf2ff8caff4814e20f03f77012fae0e42
tobeyadr <gO0UsDYxRwhz>
2019-08-06 11:37:11 -0400

    add num args

--- 2.0/tag-manipulation.php
+++ 2.0/tag-manipulation.php
@@ -3,8 +3,14 @@
 define( 'ACTIVE_TAG_ID', 1234 ); // todo change tag id
 define( 'INACTIVE_TAG_ID', 4321 ); // todo change tag id
 
-add_action( 'groundhogg/contact/tag_applied', function ( \Groundhogg\Contact $contact, int $tag_id ){
+add_action( 'groundhogg/contact/tag_applied', 'switch_tags_when_changed', 10, 2 );
 
+/**
+ * @param $contact \Groundhogg\Contact
+ * @param $tag_id int
+ */
+function switch_tags_when_changed( $contact, $tag_id )
+{
     // Ensure that a contact only has 1 of these tags at any given time.
     switch ( $tag_id ){
         case ACTIVE_TAG_ID:
@@ -14,5 +20,4 @@ add_action( 'groundhogg/contact/tag_applied', function ( \Groundhogg\Contact $co
             $contact->remove_tag( ACTIVE_TAG_ID );
             break;
     }
-
-} );
+}
